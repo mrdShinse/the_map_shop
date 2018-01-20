4bbd0ea8cb46302c055cbcd606c399ff607764a1
mrdShinse <shinse1128@hotmail.com>
2018-01-20 16:41:54 +0900

    install devise ``` bundle exec rails generate devise:install bundle exec rails g devise:views ```

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# auth
+gem 'devise'
+
 # view
 gem 'slim-rails'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,10 +28,13 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # ActionMailer
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
+  # Devise
+  # see this page. https://github.com/plataformatec/devise/tree/v4.4.0#getting-started
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
