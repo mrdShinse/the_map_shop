ed2372a0e0b644c19c37314b83023d24983968fb
mrdShinse <shinse1128@hotmail.com>
2018-02-11 19:08:44 +0900

    show owned maps on dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController # :nodoc:
-  def index; end
+  def index
+    @maps = Map.mine(current_user)
+  end
 end
--- app/models/map.rb
+++ app/models/map.rb
@@ -3,4 +3,6 @@
 class Map < ApplicationRecord # :nodoc:
   belongs_to :user
   has_many :pins, dependent: :destroy
+
+  scope :mine, ->(user) { where(user: user) }
 end
